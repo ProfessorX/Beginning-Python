ca472b00bc559c5c46ee28596bb9c3e42736e40f
Abraham Xiao <q289196573@gmail.com>
2013-11-13 02:10:09 +0400

    Emacs is so damn powerful

---
+++ Appendix-A-Loose-End.py
@@ -0,0 +1,21 @@
+# 20131113 Lab 02:00
+import math
+x = math.sqrt(y)
+
+# or something like this
+from math import sqrt
+x = sqrt(y)
+
+# You want to add something like this at the end of your program
+if __name__ == "__main__": main()
+
+# On UNIX, use the following line to make it run by itself
+#!/usr/bin/env python
+
+# You can aviod some problems by try/except statement
+def safe_division(a, b):
+    try:
+        return a/b
+    except:
+        ZeroDivisionError
+
