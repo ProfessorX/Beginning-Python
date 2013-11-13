d2492d3141c9b846d74bf2d81c97c4916c6bd832
Abraham Xiao <q289196573@gmail.com>
2013-11-14 01:11:42 +0400

    Sleeping time. The Chinese saying is that early to bed and early up, make you healthy, wealthy and wise.

---
+++ Python-Reference.py
@@ -0,0 +1,7 @@
+# 20131113 Lab 21:30
+
+# Expression statements
+print "This module contains SPAM-related functions."
+
+# Assert statements
+assert age >= 12, "Children under the age 12 are not allowed!"
