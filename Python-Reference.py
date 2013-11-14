b3ca06536a39072ba9cbb18613dd74c3c948d2bc
Abraham XIao <q289196573@Gmail.com>
2013-11-14 20:25:00 +0400

    *** empty log message ***

--- Python-Reference.py
+++ Python-Reference.py
@@ -5,3 +5,25 @@ print "This module contains SPAM-related functions."
 
 # Assert statements
 assert age >= 12, "Children under the age 12 are not allowed!"
+
+# Assignment Statements
+x = 42  # Simple assignment
+name, age = 'Gumby', 60  # Sequence unpacking
+x = y = z = 10  # Chained assignments
+
+# Augmented assignment statements
+x *= 2  # Double x
+x += 5  # adds 5 to x
+
+# The pass statement
+try x.name
+except: AttributeError:
+    pass
+else:
+    print "Hello", x.name
+
+del x  # ubinds a variable
+del seq[42]  # delete a sequence item
+del seq[42:]  # delete a sequence slice  
+del map['foo']  # delete a mapping item
+
