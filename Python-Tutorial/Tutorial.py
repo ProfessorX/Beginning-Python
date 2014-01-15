30b5b64beb733e191ad64f6320aa2cd2696fdf9b
Abraham Xiao <q289196573@Gmail.com>
2014-01-15 12:55:23 +0400

    Someone like you. Either you work hard and be outstanding, which is not so easy. or you get fucked by the society and get doomed.

---
+++ Python-Tutorial/Tutorial.py
@@ -0,0 +1,69 @@
+2 * 1024
+3 + 4 + 9
+
+for n in range(1,5):
+    print(n, n**2)
+
+from math import sqrt,exp
+exp(-1)
+
+sqrt(2)
+
+from math import *
+
+def square(x):
+    return x*x
+
+square(2)
+
+square(2.5)
+
+print("Hello, Masdar")
+
+[1,3,4,1,6]
+
+(1,3,2)
+
+[1,2, "Hello,", 3, 4]
+
+A = [1,2, "Hello,", 3, 4]
+
+list(range(1,17,2))
+
+list(range(1,10))  # something we have to notice is, the list is half-open half closed. As we can see from the output.
+
+from Tutorial import square
+
+a = ["someone", 1, 2, 3, ("like", 998), {abe: 998, Bitch: 2046}]
+
+{k: 1, j: 2}
+
+x = "Someone like you."
+len(x)
+
+D = {'Food': 'sucks', 'lifeF': 'great'}
+
+x = 1
+if x < 0:
+    print("That is life.")
+else:
+    print("Why not earn a Ph.D.?")
+
+for i in range(2,10):
+    for j in range(2,i):
+        if i%j==0:
+            print(i, 'equals', j, '*', i//j)
+            break
+        else:
+            # something wrong here
+            print(i)
+
+
+
+
+        
+    
+    
+
+
+
